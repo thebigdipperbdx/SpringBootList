2ed8a49b8ad6a62a763dbcf833585a78e8f35463
yanyg <thebigdipperbdx@126.com>
2019-10-12 13:18:45 +0800

    自定义线程池

--- list-demo/src/main/java/com/list/TestSingleton.java
+++ list-demo/src/main/java/com/list/TestSingleton.java
@@ -4,7 +4,9 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author yanyugang
@@ -12,15 +14,18 @@ import java.util.concurrent.Executors;
  * @date 2019-10-09 14:56
  */
 public class TestSingleton {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException{
 
-        final Set<String> set = Collections.synchronizedSet(new HashSet<String>());
-        ExecutorService executor = Executors.newCachedThreadPool();
-        for (int i = 0; i < 1000; i++) {
+        final Set<String> set=Collections.synchronizedSet(new HashSet<String>());
+        // 自定义线程池
+        ExecutorService executor=new ThreadPoolExecutor(100, 500,
+                60L, TimeUnit.SECONDS,
+                new SynchronousQueue<Runnable>());
+        for (int i=0; i < 1000; i++) {
             executor.execute(new Runnable() {
                 @Override
-                public void run() {
-                    Singleton singleton = Singleton.getInstance();
+                public void run(){
+                    Singleton singleton=Singleton.getInstance();
                     set.add(singleton.toString());
                 }
             });
