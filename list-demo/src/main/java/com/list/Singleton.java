1f4e8821ed71c8527fc0fca89174cf45ea86f04e
yanyg <thebigdipperbdx@126.com>
2019-10-17 17:24:21 +0800

    双重校验锁

--- list-demo/src/main/java/com/list/Singleton.java
+++ list-demo/src/main/java/com/list/Singleton.java
@@ -2,21 +2,20 @@ package com.list;
 
 /**
  * @author yanyugang
- * @description
+ * @description 双重校验锁（DCL）
  * @date 2019-10-10 9:06
  */
 public class Singleton {
     private static volatile Singleton singleton;
-
-    private Singleton(){
-    }
-
-    public static Singleton getInstance(){
-        if (singleton==null){
-            // 同步块，给当前对象实例加锁
+    // 防止反射创建对象
+    private Singleton (){}
+    // 静态方法
+    public static Singleton getInstance() {
+        if (singleton == null) {
+            // 对当前类加锁
             synchronized (Singleton.class) {
-                if (singleton==null){
-                    singleton=new Singleton();
+                if (singleton == null) {
+                    singleton = new Singleton();
                 }
             }
         }
