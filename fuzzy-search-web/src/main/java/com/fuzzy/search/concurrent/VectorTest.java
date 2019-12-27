bb21f33e7c21411ae4cef7d7e9eb7e3f67a89776
yanyg <thebigdipperbdx@126.com>
2019-12-27 09:15:24 +0800

    代码提交

--- fuzzy-search-web/src/main/java/com/fuzzy/search/concurrent/VectorTest.java
+++ fuzzy-search-web/src/main/java/com/fuzzy/search/concurrent/VectorTest.java
@@ -17,23 +17,21 @@ public class VectorTest {
                 vector.add(i);
             }
 
-            Thread removeThread=new Thread(new Runnable() {
-                @Override
-                public void run(){
-                    synchronized (vector){
-                        for (int i=0; i < vector.size(); i++) {
-                            vector.remove(i);
-                        }
+            Thread removeThread=new Thread(() -> {
+                synchronized (vector){
+                    for (int i=0; i < vector.size(); i++) {
+                        vector.remove(i);
                     }
                 }
             });
-            Thread printThread=new Thread(new Runnable() {
-                @Override
-                public void run(){
-                    synchronized (vector){
-                        for (int i=0; i < vector.size(); i++) {
-                            System.out.println(vector.get(i));
-                        }
+
+
+
+
+            Thread printThread=new Thread(() -> {
+                synchronized (vector){
+                    for (int i=0; i < vector.size(); i++) {
+                        System.out.println(vector.get(i));
                     }
                 }
             });
--- fuzzy-search-web/src/main/java/com/fuzzy/search/concurrent/VolatileScene.java
+++ fuzzy-search-web/src/main/java/com/fuzzy/search/concurrent/VolatileScene.java
@@ -3,6 +3,7 @@ package com.fuzzy.search.concurrent;
 /**
  * @author yanyugang
  * @description volatile的使用场景
+ * 保证可见性、有序性
  * @date 2019-08-06 13:15
  */
 public class VolatileScene {
@@ -13,7 +14,8 @@ public class VolatileScene {
     }
 
     public void doWork(){
-        while (!shutdownRequested) {//do stuff
+        //do stuff
+        while (!shutdownRequested) {
         }
     }
 }
