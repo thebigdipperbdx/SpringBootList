184fd1638d4758b40e8cb030b92510190e7c0a28
thebigdipperbdx <thebigdipperbdx@126.com>
2020-01-08 21:51:37 +0800

    代码修改

--- fuzzy-search-web/src/main/java/com/fuzzy/search/aop/LogAspect.java
+++ fuzzy-search-web/src/main/java/com/fuzzy/search/aop/LogAspect.java
@@ -9,15 +9,13 @@ import org.springframework.stereotype.Component;
 
 /**
  * @author yanyugang
- * @description
- * 官方配置文档：
+ * @description 官方配置文档：
  * https://docs.spring.io/spring/docs/5.1.13.BUILD-SNAPSHOT/spring-framework-reference/core.html#aop-ataspectj
  * @date 2019-12-31 9:50
  */
 @Aspect
 @Component
 public class LogAspect {
-
 
     // 切点表达式限制为包含指定注解RequestMapping的连接点（配置方式一）
     @Pointcut("@annotation(org.springframework.web.bind.annotation.RequestMapping)")
@@ -27,18 +25,21 @@ public class LogAspect {
     @Around("cutPoint()")
     public Object bizOnPoint(ProceedingJoinPoint joinPoint) throws Throwable {
         System.out.println("前置通知（@Before的作用）");
-        Object result=null;
+        Object result = null;
         try {
             // Returns the arguments at this join point.
             // 拦截的方法的参数
-            Object[] args=joinPoint.getArgs();
+            Object[] args = joinPoint.getArgs();
             // 方法的相关信息，方法名等
-            Signature signature=joinPoint.getSignature();
+            Signature signature = joinPoint.getSignature();
             // 方法名
-            String name=signature.getName();
-            int modifiers=signature.getModifiers();
+            String name = signature.getName();
+            int modifiers = signature.getModifiers();
+            long start = System.currentTimeMillis();
             // 执行方法
-            result=joinPoint.proceed();
+            result = joinPoint.proceed();
+            long end = System.currentTimeMillis();
+            System.out.println(name + "方法执行耗时=====>" + (end - start) + "ms");
             System.out.println("返回通知（@AfterReturning的作用）");
         } catch (Throwable throwable) {
             throwable.printStackTrace();
@@ -58,9 +59,9 @@ public class LogAspect {
     public Object bizMyPoint(ProceedingJoinPoint joinPoint) throws Throwable {
         System.out.println("bizMyPoint");
         System.out.println("前置通知（@Before的作用）");
-        Object result=null;
+        Object result = null;
         try {
-            result=joinPoint.proceed();
+            result = joinPoint.proceed();
             System.out.println("返回通知（@AfterReturning的作用）");
         } catch (Throwable throwable) {
             throwable.printStackTrace();
